f3a761c51143f2da895ad88b00c643f75585d460
Driverless Computer <cfsautonomous@gmail.com>
2025-05-25 13:52:05 +0200

    plugins

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -9,6 +9,11 @@ then
 	exit 1
 fi
 
-cp -R -u -p ~/dotfiles/zsh/zshrc ~/.zshrc
-cp -R -u -p ~/dotfiles/zsh/zsh_aliases ~/.zsh_aliases
+cp -p ~/dotfiles/zsh/zshrc ~/.zshrc
+cp -p ~/dotfiles/zsh/zsh_aliases ~/.zsh_aliases
 
+# nice to have plugins
+# echo getting plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
