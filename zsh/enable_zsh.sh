21aa37637586d15cc332b40aa42400aed9e500de
ArthurAlexandersson <arthuralexandersson@hotmail.se>
2025-05-25 13:23:03 +0200

    updates to zsh, added install script

---
+++ zsh/enable_zsh.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Do you want to install zsh [y/n] : " 
+read input1
+
+if [ "$input1" = "y" ]
+then
+	echo "installing zsh"
+	sudo apt update
+	sudo apt install zsh -y
+else
+	echo "exiting"
+	exit 1
+fi
+
+chsh -s $(which zsh)
+
+echo  "Do you want to install ohmyzsh [y/n] : "
+read input2
+if [ "$input2" = 'y' ]
+then
+	sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+else
+	echo "exiting"
+	exit 1
+fi 
+
+
