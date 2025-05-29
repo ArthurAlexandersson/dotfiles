182253349c3354799023eb6803681a5902fb00eb
ArthurAlexandersson <arthuralexandersson@hotmail.se>
2025-05-29 13:30:24 +0200

    made formatting kindof work

--- nvim/init.lua
+++ nvim/init.lua
@@ -640,7 +640,7 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = {}
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
 				else
@@ -652,11 +652,28 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				c = { "uncrustify" },
+				cpp = { "uncrustify" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			formatters = {
+				uncrustify = {
+					command = "uncrustify",
+					stdin = false,
+					args = {
+						"-c",
+						vim.fn.expand("~/dotfiles/linters/cfs_code_style.cfg"),
+						"--frag",
+						"-f",
+						vim.api.nvim_buf_get_name(0),
+						"-o",
+						vim.api.nvim_buf_get_name(0),
+					},
+				},
 			},
 		},
 	},
