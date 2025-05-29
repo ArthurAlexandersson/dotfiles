4a8144fd0de9ee0f945e0dd7ce4c1601b5cb6cf9
ArthurAlexandersson <arthuralexandersson@hotmail.se>
2025-05-29 13:29:33 +0200

    changes

--- wezterm/setup_wezterm.sh
+++ wezterm/setup_wezterm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Setting up wezterm config"
+
 if ! command -v wezterm 2>&1 >/dev/null
 then
     echo "wezterm could not be found"
@@ -8,8 +9,16 @@ then
     echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
     sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
     sudo apt update
-    sudo apt install wezterm-nightly
+
+    ARCH=$(uname -m)
+
+    if["$ARCH" = "aarch64"] || ["$ARCH" = "arm64"]; then
+        sudo apt install wezterm
+    else
+        sudo apt install wezterm-nightly
 fi
+
+
 
 cp -R -u -p ~/dotfiles/wezterm ~/.config
 
