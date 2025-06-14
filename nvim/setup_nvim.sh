76fc4f7241e6924af86c3deba5c423512c4e0633
ArthurAlexandersson <arthuralexandersson@hotmail.se>
2025-06-14 12:37:24 +0200

    setup.sh

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -7,7 +7,11 @@ DEST="$HOME/.config/nvim"
 # Create destination directory if it doesn't exist
 mkdir -p "$DEST"
 
-# Copy all files and directories, preserving permissions and symbolic links
-cp -rT "$SRC" "$DEST"
+# Clear destination to mimic cp -rT behavior (replacing DEST with SRC content)
+rm -rf "$DEST"/*
+rm -rf "$DEST"/.??* 2>/dev/null  
+
+# Copy all files and directories, preserving attributes and symlinks
+cp -a "$SRC"/. "$DEST"
 
 echo "Nvim setup copied from $SRC to $DEST"
